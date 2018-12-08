b6e704d0ad61b5cb3221a3fa937e7134368084a8
Makc711 <40514121+Makc711@users.noreply.github.com>
2018-12-08 16:13:15 +0700

    Решение задания 3.6

---
+++ Task_3_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_3_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Задан текст. Определить,содержит ли он цифры?";
+
+            Console.WriteLine(IsTextContainNumber(text) ? "да" : "нет");
+            Console.ReadKey();
+        }
+
+        public static bool IsTextContainNumber(string text)
+        {
+            foreach (var c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
